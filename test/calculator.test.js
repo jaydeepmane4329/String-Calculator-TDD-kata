c980fedf181bd71035707a81e320f21ed5094797
jaydeep <jaydeepmane4329@gmail.com>
2024-09-29 14:22:20 +0530

    handling sum of two comma-separated numbers

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -6,5 +6,8 @@ test('should return 0 for an empty string', () => {
 
 test('should return the number itself when only one number is present', () => {
     expect(add('1')).toBe(1);
-  });
-  
+});
+
+test('should return the sum of two comma-separated numbers', () => {
+    expect(add('1,2')).toBe(3);
+});
