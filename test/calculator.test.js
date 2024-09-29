4cc16ef02ef27e5f306c84416082d441bb5252dc
jaydeep <jaydeepmane4329@gmail.com>
2024-09-29 14:40:32 +0530

    handling multiple negative numbers

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -28,4 +28,8 @@ test('should throw an exception for negative numbers', () => {
     expect(() => add('1,-2,3')).toThrow('negative numbers not allowed -2');
 });
 
+test('should throw an exception for multiple negative numbers', () => {
+    expect(() => add('1,-2,-3')).toThrow('negative numbers not allowed -2,-3');
+});
+
 
