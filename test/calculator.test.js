96430578488528ec9ada7000be1b6416d23cb311
jaydeep <jaydeepmane4329@gmail.com>
2024-09-29 14:18:10 +0530

    handling test case for single number

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,3 +3,8 @@ const { add } = require('../src/calculator');
 test('should return 0 for an empty string', () => {
     expect(add('')).toBe(0);
 });
+
+test('should return the number itself when only one number is present', () => {
+    expect(add('1')).toBe(1);
+  });
+  
